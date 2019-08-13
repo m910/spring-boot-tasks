f4ca266f54c4e9d200bac9470f164473ee3fce24
m910 <52059926+m910@users.noreply.github.com>
2019-08-13 09:42:01 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -1,6 +1,8 @@
 package com.stackroute.service;
 
 import com.stackroute.domain.Track;
+import com.stackroute.exceptions.TrackAlreadyExistsException;
+import com.stackroute.exceptions.TrackNotFoundException;
 import com.stackroute.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,41 +23,85 @@ public class TrackServiceImpl implements TrackService {
 
 
     @Override
-    public Track saveTrack(Track track) {
+    public Track saveTrack(Track track) throws TrackAlreadyExistsException {
 
-        Track saveTrack= trackRepository.save(track);
+        if(trackRepository.existsById(track.getId())){
+            throw new TrackAlreadyExistsException("user already exists exception");
+        }
+
+        Track saveTrack=trackRepository.save(track);
+        if(saveTrack==null){
+            throw new TrackAlreadyExistsException("user already exists");
+        }
         return saveTrack;
     }
 
+
     @Override
-    public Track getTrackById(int id) {
-        Track retrivedTrack = trackRepository.findById(id).get();
-        return retrivedTrack;
+    public Track getTrackById(int id) throws TrackNotFoundException{
+
+        if(!trackRepository.existsById(id)){
+            throw new TrackNotFoundException("track not found exception");
+        }
+        Track getTrackById= trackRepository.getOne(id);
+        if(getTrackById==null) {
+
+            throw new TrackNotFoundException("track not found");
+        }
+        return getTrackById;
     }
 
     @Override
-    public List<Track> getAllTracks() {
-        return trackRepository.findAll();
+    public List<Track> getAllTracks() throws Exception {
+        if (trackRepository.findAll().isEmpty()) {
+
+            throw new Exception("exception");
+        }
+        return getAllTracks();
+    }
+
+
+    @Override
+    public Track deleteTrackById(int id) throws TrackNotFoundException {
+        if(trackRepository.existsById(id)){
+            throw new TrackNotFoundException("track not found exception");
+        }
+        Track deleteTrackById= trackRepository.getOne(id);
+        if(deleteTrackById==null) {
+
+            throw new TrackNotFoundException("track not found");
+        }
+        return deleteTrackById;
+
+
     }
 
     @Override
-    public Track deleteTrackById(int id) {
-         Optional<Track> optionalTrack= trackRepository.findById(id);
-        trackRepository.deleteById(id);
-         return optionalTrack.get();
+    public Track updateTrackById(int id,Track track) throws TrackNotFoundException {
+        if(trackRepository.existsById(id)){
+            throw new TrackNotFoundException("track not found exception");
+        }
+        Track updateTrackById= trackRepository.getOne(id);
+        if(updateTrackById==null) {
+
+            throw new TrackNotFoundException("track not found");
+        }
+        return updateTrackById;
+
     }
 
     @Override
-    public Track updateTrackById(int id,Track track) {
-        Optional<Track> optionalTrack = trackRepository.findById(id);
-        Track updateTrack= trackRepository.save(track);
-        return updateTrack;
+    public List<Track> getTrackByName(String name) throws TrackNotFoundException {
+        if (trackRepository.findAll().isEmpty()) {
 
-    }
+            throw new TrackNotFoundException("track not found");
+        }
+        Track getTrackByName = (Track) trackRepository.getTrackByName(name);
+        if(getTrackByName==null) {
 
-    @Override
-    public List<Track> getTracksByName(List<String> name) {
-        List<Track> listOfTracks = trackRepository.findByName(name);
-        return listOfTracks;
+            throw new TrackNotFoundException("track not found");
+        }
+        return (List<Track>) getTrackByName;
+
     }
 }
